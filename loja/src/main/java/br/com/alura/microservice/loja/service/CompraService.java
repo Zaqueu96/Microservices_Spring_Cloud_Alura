1a458a2cbb8586357f0b39a4738f10fed45404ea
Zaqueu PS <zaqueu96@hotmail.com>
2021-09-18 16:01:11 -0300

    feat: created method of bean to get ip by name

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,13 @@ import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+	
 	public void realizaCompra(CompraDTO compra) {
 		
-		RestTemplate client = new RestTemplate();
 		 ResponseEntity<InfoFornecedorDTO> infoFornecedor = 
-				 client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
+				 this.client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
 				HttpMethod.GET,null,InfoFornecedorDTO.class);
 		 
 		 System.out.println(infoFornecedor.getBody().getEndereco());
